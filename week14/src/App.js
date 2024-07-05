39b05d64d6cc55f2f20075e8518c432310ba01d2
kimtree24 <ktj503@naver.com>
2024-07-05 15:41:00 +0900

    feat: 세션 내용

---
+++ week14/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Signup from './pages/Signup';
+import Mypage from './pages/Mypage';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path = "/" element={<Home />}></Route>
+      <Route path = "/signup" element={<Signup />}></Route>
+      <Route path = "/mypage" element={<Mypage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
